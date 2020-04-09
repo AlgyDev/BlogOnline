7e151d05e489be714d94b326168fe608c03e2326
Zerok <algy20yt@gmail.com>
2020-04-09 16:10:08 +0200

    Blog
    Blog con registro de usuarios, publicar post, comentar, etc.

---
+++ app/config.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+$nombre_servidor = 'localhost';
+$nombre_usuario = 'root';
+$password = '';
+$nombre_base_de_datos = 'blog';
---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/estilos.css" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-default navbar-static-top">
+
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Éste botón despliega la barra de navegación</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="">Inicio</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Entradas</a></li>
+                    <li><a href="#">Favoritos</a></li>
+                    <li><a href="#">Autores</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Iniciar sesión</a></li>
+                    <li><a href="#">Registro</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Blog creación</h1>
+            <p>
+                En proceso.
+            </p>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <span class="glyphicon glyphicon-search" aria-hidden="true"></span> Búsqueda
+                            </div>
+                            <div class="panel-body">
+                                <div class="form-group">
+                                    <input type="search" class="form-control" placeholder="¿Qué buscas?">
+                                </div>
+                                <button class="form-control">Buscar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <span class="glyphicon glyphicon-filter" aria-hidden="true"></span> Filtro
+                            </div>
+                            <div class="panel-body">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> Archivo
+                            </div>
+                            <div class="panel-body">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <span class="glyphicon glyphicon-time" aria-hidden="true"></span> Últimas entradas
+                    </div>
+                    <div class="panel-body">
+                        <p>Todavía no hay entradas para mostrar.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
